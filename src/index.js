565d1f740818b77a6047ae7d46f40d4a2c0dae08
Azam <gulovazam@gmail.com>
2023-04-22 17:09:23 +0500

    Add current weather render method

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import screenController from "./modules/screenController";
 import {
+  currentForecastCard,
   dailyForecastCardGenerator,
   hourlyForecastCardGenerator,
 } from "./modules/forecastCard";
 
+const currentWeatherSection = document.querySelector(".current-weather");
+const city = currentWeatherSection.querySelector(".city");
+const chanceOfRain = currentWeatherSection.querySelector(
+  ".chance-of-rain-perc"
+);
+
+const currentTemp = currentWeatherSection.querySelector(".temp");
 const weeklyForecastAside = document.querySelector(".weekly-forecast");
 const hourlyForecastSection = document.querySelector(".hourly-forecast");
 
 const app = screenController();
+currentForecastCard(city, chanceOfRain, currentTemp);
 dailyForecastCardGenerator.render(weeklyForecastAside);
 hourlyForecastCardGenerator.render(hourlyForecastSection);
