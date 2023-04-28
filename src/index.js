be4fa1df98cbb981f0cdb8219b52cc5da70e1a27
Azam <gulovazam@gmail.com>
2023-04-28 10:30:47 +0500

    render air conditions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import screenController from "./modules/screenController";
 import {
   currentForecastCard,
+  airConditionGenerator,
   dailyForecastCardGenerator,
   hourlyForecastCardGenerator,
 } from "./modules/forecastCard";
@@ -10,12 +11,16 @@ const city = currentWeatherSection.querySelector(".city");
 const chanceOfRain = currentWeatherSection.querySelector(
   ".chance-of-rain-perc"
 );
-
 const currentTemp = currentWeatherSection.querySelector(".temp");
+const realFeel = document.querySelector(".real-feel");
+const wind = document.querySelector(".wind");
+const humidity = document.querySelector(".humidity");
+const uvIndex = document.querySelector(".uv-index");
 const weeklyForecastAside = document.querySelector(".weekly-forecast");
 const hourlyForecastSection = document.querySelector(".hourly-forecast");
 
 const app = screenController();
 currentForecastCard(city, chanceOfRain, currentTemp);
+airConditionGenerator.render(realFeel, humidity, wind, uvIndex);
 dailyForecastCardGenerator.render(weeklyForecastAside);
 hourlyForecastCardGenerator.render(hourlyForecastSection);
