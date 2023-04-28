1562502dfe6d3ccebceb8d55b0c812863e51f34e
Azam <gulovazam@gmail.com>
2023-04-28 17:28:49 +0500

    screen controller publishes search value
    Move methods to their related components

--- src/modules/screenController.js
+++ src/modules/screenController.js
@@ -1,39 +1,15 @@
-import forecastController from "./forecastController";
+import pubsub from "./pubsub";
 
 const screenController = () => {
   const searchForm = document.querySelector("form");
   const { locationSearch } = searchForm;
-  const error = searchForm.querySelector(".error");
-  const loader = document.querySelector(".loader-container");
-  // const currTempIcon = currentWeatherSection.querySelector(".weather-icon");
   searchForm.addEventListener("submit", renderData);
 
-  async function renderData(e) {
+  function renderData(e) {
     e.preventDefault();
     const searchValue = locationSearch.value;
     searchForm.reset();
-    resetErrorHandler();
-    loader.dataset.state = "displayed";
-
-    try {
-      await forecastController.getForecast(searchValue);
-    } catch (err) {
-      errorHandler(err.message);
-    } finally {
-      loader.dataset.state = "hidden";
-    }
-  }
-
-  function errorHandler(err) {
-    error.textContent = err;
-    error.dataset.state = "displayed";
-    error.setAttribute("aria-hidden", "false");
-  }
-
-  function resetErrorHandler() {
-    error.textContent = "";
-    error.dataset.state = "hidden";
-    error.setAttribute("aria-hidden", "true");
+    pubsub.publish("search", searchValue);
   }
 };
 
