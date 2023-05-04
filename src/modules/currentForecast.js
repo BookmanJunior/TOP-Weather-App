dd1ed1080e6a32690fe685606c80966cce6a88f3
Azam <gulovazam@gmail.com>
2023-05-04 17:11:07 +0500

    add methods change units
    listens for unit change

--- src/modules/currentForecast.js
+++ src/modules/currentForecast.js
@@ -1,6 +1,8 @@
 import pubsub from "./pubsub";
 
 const renderCurrentForecast = (city, rain, temp) => {
+  let forecastData;
+
   const filterForecastData = (data) => {
     const location = data.location.name;
     const chanceOfRain = data.forecast.forecastday[0].day.daily_chance_of_rain;
@@ -8,17 +10,28 @@ const renderCurrentForecast = (city, rain, temp) => {
     const tempF = data.current.temp_f;
     const condition = data.current.condition.text;
     const filteredData = { location, chanceOfRain, tempC, tempF, condition };
+    forecastData = filteredData;
     return filteredData;
   };
 
   function render(data) {
-    const forecastData = filterForecastData(data);
-    city.textContent = forecastData.location;
-    rain.textContent = `${forecastData.chanceOfRain}%`;
+    const currForecast = filterForecastData(data);
+    city.textContent = currForecast.location;
+    rain.textContent = `${currForecast.chanceOfRain}%`;
+    temp.textContent = `${currForecast.tempC}°C`;
+  }
+
+  function changeUnitsToImperial() {
+    temp.textContent = `${forecastData.tempF}°F`;
+  }
+
+  function changeUnitsToMetric() {
     temp.textContent = `${forecastData.tempC}°C`;
   }
 
   pubsub.sub("fetchedForecast", render);
+  pubsub.sub("changeUnitsToImperial", changeUnitsToImperial);
+  pubsub.sub("changeUnitsToMetric", changeUnitsToMetric);
 };
 
 export default renderCurrentForecast;
