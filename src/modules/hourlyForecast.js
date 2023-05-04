6a24cde6e5b4c1dc74dd00db70fd0011eb346aed
Azam <gulovazam@gmail.com>
2023-05-04 17:11:53 +0500

    Add functionality to change units
    sub to unit change

--- src/modules/hourlyForecast.js
+++ src/modules/hourlyForecast.js
@@ -3,6 +3,8 @@ import pubsub from "./pubsub";
 import elementCreator from "./elementCreator";
 
 const hourlyForecastCard = () => {
+  let forecastData;
+
   function filterForecastData(data) {
     const hourlyForecast = data.forecast.forecastday[0].hour;
     const desiredHours = [6, 9, 12, 13, 18, 21];
@@ -13,11 +15,13 @@ const hourlyForecastCard = () => {
     const filteredHourlyForecast = filteredHours.reduce((newArr, currItem) => {
       const { time } = currItem;
       const condition = currItem.condition.text;
-      const temp = Math.round(currItem.temp_c);
-      const hourlyData = { time, condition, temp };
+      const tempC = currItem.temp_c;
+      const tempF = currItem.temp_f;
+      const hourlyData = { time, condition, tempC, tempF };
       newArr.push(hourlyData);
       return newArr;
     }, []);
+    forecastData = filteredHourlyForecast;
     return filteredHourlyForecast;
   }
 
@@ -28,7 +32,7 @@ const hourlyForecastCard = () => {
     );
     cardContainer.textContent = "";
     filteredForecastData.forEach((data) => {
-      const card = hourlyCardEl(data.time, data.condition, data.temp);
+      const card = hourlyCardEl(data.time, data.condition, data.tempC);
       cardContainer.appendChild(card);
     });
   };
@@ -39,6 +43,23 @@ const hourlyForecastCard = () => {
 
     pubsub.sub("fetchedForecast", generateHourlyCard);
   };
+
+  function changeUnitsToImperial() {
+    const temps = document.querySelectorAll(".hourly-temp");
+    temps.forEach((temp, index) => {
+      temp.textContent = `${forecastData[index].tempF}°F`;
+    });
+  }
+
+  function changeUnitsToMetric() {
+    const temps = document.querySelectorAll(".hourly-temp");
+    temps.forEach((temp, index) => {
+      temp.textContent = `${forecastData[index].tempC}°C`;
+    });
+  }
+
+  pubsub.sub("changeUnitsToImperial", changeUnitsToImperial);
+  pubsub.sub("changeUnitsToMetric", changeUnitsToMetric);
 
   function hourlyCardEl(time, condition, temp) {
     const hourlyCard = elementCreator(
