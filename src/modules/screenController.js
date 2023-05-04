b04dffa2879f97fa0e16e4b0d1692590e48080e9
Azam <gulovazam@gmail.com>
2023-05-04 17:10:01 +0500

    Add unit buttons functionality
    Rename form method

--- src/modules/screenController.js
+++ src/modules/screenController.js
@@ -3,13 +3,44 @@ import pubsub from "./pubsub";
 const screenController = () => {
   const searchForm = document.querySelector("form");
   const { locationSearch } = searchForm;
-  searchForm.addEventListener("submit", renderData);
+  const tempCBtn = document.querySelector(".tempC-btn");
+  const tempFBtn = document.querySelector(".tempF-btn");
 
-  function renderData(e) {
+  searchForm.addEventListener("submit", publishSearchValue);
+
+  tempCBtn.addEventListener("click", () => {
+    toggleTempButton(tempCBtn);
+    pubsub.publish("changeUnitsToMetric", "c");
+  });
+
+  tempFBtn.addEventListener("click", () => {
+    toggleTempButton(tempFBtn);
+    pubsub.publish("changeUnitsToImperial", "f");
+  });
+
+  function publishSearchValue(e) {
     e.preventDefault();
     const searchValue = locationSearch.value;
     searchForm.reset();
     pubsub.publish("search", searchValue);
+  }
+
+  function toggleTempButton(btn) {
+    if (isButtonActive(btn)) {
+      return;
+    }
+    removeCurrActiveBtnClass();
+    btn.classList.add("active-temp-unit");
+  }
+
+  function isButtonActive(btn) {
+    const isBtn = btn.classList.contains("active-temp-unit") === true;
+    return isBtn;
+  }
+
+  function removeCurrActiveBtnClass() {
+    const currentActiveTempBtn = document.querySelector(".active-temp-unit");
+    currentActiveTempBtn.classList.remove("active-temp-unit");
   }
 };
 
